f7c9db80866029faa5ada82a4e71af9e4c7f0f30
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-01-24 21:39:17 -0300

    Sistema de Compra y Venta

--- inv/views.py
+++ inv/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Categoria
 
-# Create your views here.
+class CategoriaView(LoginRequiredMixin, generic.ListView):
+    model = Categoria
+    template_name = 'inv/categoria_list.html'
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
