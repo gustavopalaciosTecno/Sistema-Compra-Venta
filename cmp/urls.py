02b2c3aa2019e13951d347c5ce10611391502d3f
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-08-31 00:15:51 -0300

    Solución a la opción de compra

--- cmp/urls.py
+++ cmp/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path, include
-from .views import ProveedorView, ProveedorNew, ProveedorEdit, \
+
+from .views import ProveedorView,ProveedorNew, ProveedorEdit, \
     proveedorInactivar, \
-    ComprasView, compras    
+    ComprasView, compras, CompraDetDelete
+
+# from .reportes import reporte_compras, imprimir_compra 
 
 urlpatterns = [
-    path('proveedores/', ProveedorView.as_view(), name='proveedor_list'),
-    path('proveedores/new', ProveedorNew.as_view(), name='proveedor_new'),
-    path('proveedores/edit/<int:pk>', ProveedorEdit.as_view(), name='proveedor_edit'),
-    path('proveedores/inactvar/<int:id>', proveedorInactivar, name='proveedor_inactivar'),
+    path('proveedores/',ProveedorView.as_view(), name="proveedor_list"),
+    path('proveedores/new',ProveedorNew.as_view(), name="proveedor_new"),
+    path('proveedores/edit/<int:pk>',ProveedorEdit.as_view(), name="proveedor_edit"),
+    path('proveedores/inactivar/<int:id>',proveedorInactivar, name="proveedor_inactivar"),
+
     path('compras/',ComprasView.as_view(), name="compras_list"),
-    path('compras/new',compras,name='compras_new'),
-    
+    path('compras/new',compras, name="compras_new"),
+    path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
+    path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
+
+    # path('compras/listado', reporte_compras, name='compras_print_all'),
+    # path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
 ]
--- cmp/views.py
+++ cmp/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
 from django.urls import reverse_lazy
 import datetime
+from django.http import HttpResponse, JsonResponse
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 import json
+from django.db.models import Sum
 
 from .models import Proveedor, ComprasEnc, ComprasDet
-from cmp.forms import ProveedorForm, ComprasEncForm
+from cmp.forms import ProveedorForm,ComprasEncForm
 from bases.views import SinPrivilegios
 from inv.models import Producto
- 
+
+
 class ProveedorView(SinPrivilegios, generic.ListView):
     model = Proveedor
     template_name = "cmp/proveedor_list.html"
     context_object_name = "obj"
     permission_required="cmp.view_proveedor"
-    
 
 class ProveedorNew(SuccessMessageMixin, SinPrivilegios,\
                    generic.CreateView):
@@ -35,7 +36,8 @@ class ProveedorNew(SuccessMessageMixin, SinPrivilegios,\
         form.instance.uc = self.request.user
         #print(self.request.user.id)
         return super().form_valid(form)
-    
+
+
 class ProveedorEdit(SuccessMessageMixin, SinPrivilegios,\
                    generic.UpdateView):
     model=Proveedor
@@ -49,9 +51,9 @@ class ProveedorEdit(SuccessMessageMixin, SinPrivilegios,\
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         print(self.request.user.id)
-        return super().form_valid(form)    
-    
- 
+        return super().form_valid(form)
+
+
 @login_required(login_url="/login/")
 @permission_required("cmp.change_proveedor",login_url="/login/")
 def proveedorInactivar(request,id):
@@ -73,25 +75,26 @@ def proveedorInactivar(request,id):
 
     return render(request,template_name,contexto)
 
+
 class ComprasView(SinPrivilegios, generic.ListView):
     model = ComprasEnc
     template_name = "cmp/compras_list.html"
     context_object_name = "obj"
     permission_required="cmp.view_comprasenc"
-    
+
 
 @login_required(login_url='/login/')
-@permission_required('cmp.view_comprasenc', login_url='bases:sin_privilegios')    
+@permission_required('cmp.view_comprasenc', login_url='bases:sin_privilegios')
 def compras(request,compra_id=None):
-    template_name = 'cmp/compras.html'
+    template_name="cmp/compras.html"
     prod=Producto.objects.filter(estado=True)
     form_compras={}
     contexto={}
-    
+
     if request.method=='GET':
         form_compras=ComprasEncForm()
         enc = ComprasEnc.objects.filter(pk=compra_id).first()
-        
+
         if enc:
             det = ComprasDet.objects.filter(compra=enc)
             fecha_compra = datetime.date.isoformat(enc.fecha_compra)
@@ -111,11 +114,84 @@ def compras(request,compra_id=None):
             det=None
         
         contexto={'productos':prod,'encabezado':enc,'detalle':det,'form_enc':form_compras}
+
+    if request.method=='POST':
+        fecha_compra = request.POST.get("fecha_compra")
+        observacion = request.POST.get("observacion")
+        no_factura = request.POST.get("no_factura")
+        fecha_factura = request.POST.get("fecha_factura")
+        proveedor = request.POST.get("proveedor")
+        sub_total = 0
+        descuento = 0
+        total = 0
+
+        if not compra_id:
+            prov=Proveedor.objects.get(pk=proveedor)
+
+            enc = ComprasEnc(
+                fecha_compra=fecha_compra,
+                observacion=observacion,
+                no_factura=no_factura,
+                fecha_factura=fecha_factura,
+                proveedor=prov,
+                uc = request.user 
+            )
+            if enc:
+                enc.save()
+                compra_id=enc.id
+        else:
+            enc=ComprasEnc.objects.filter(pk=compra_id).first()
+            if enc:
+                enc.fecha_compra = fecha_compra
+                enc.observacion = observacion
+                enc.no_factura=no_factura
+                enc.fecha_factura=fecha_factura
+                enc.um=request.user.id
+                enc.save()
+
+        if not compra_id:
+            return redirect("cmp:compras_list")
         
-        return render(request,template_name,contexto)
+        producto = request.POST.get("id_id_producto")
+        cantidad = request.POST.get("id_cantidad_detalle")
+        precio = request.POST.get("id_precio_detalle")
+        sub_total_detalle = request.POST.get("id_sub_total_detalle")
+        descuento_detalle  = request.POST.get("id_descuento_detalle")
+        total_detalle  = request.POST.get("id_total_detalle")
+
+        prod = Producto.objects.get(pk=producto)
+
+        det = ComprasDet(
+            compra=enc,
+            producto=prod,
+            cantidad=cantidad,
+            precio_prv=precio,
+            descuento=descuento_detalle,
+            costo=0,
+            uc = request.user
+        )
+
+        if det:
+            det.save()
+
+            sub_total=ComprasDet.objects.filter(compra=compra_id).aggregate(Sum('sub_total'))
+            descuento=ComprasDet.objects.filter(compra=compra_id).aggregate(Sum('descuento'))
+            enc.sub_total = sub_total["sub_total__sum"]
+            enc.descuento=descuento["descuento__sum"]
+            enc.save()
+
+        return redirect("cmp:compras_edit",compra_id=compra_id)
+
+
+    return render(request, template_name, contexto)
+
+
+class CompraDetDelete(SinPrivilegios, generic.DeleteView):
+    permission_required = "cmp.delete_comprasdet"
+    model = ComprasDet
+    template_name = "cmp/compras_det_del.html"
+    context_object_name = 'obj'
     
-        
-    
-       
-    
-    
+    def get_success_url(self):
+          compra_id=self.kwargs['compra_id']
+          return reverse_lazy('cmp:compras_edit', kwargs={'compra_id': compra_id})
